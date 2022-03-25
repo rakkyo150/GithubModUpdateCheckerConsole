858037a0cde78d2a350482d013812b7b0a95dede
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-03-25 23:29:58 +0900

    一応消しとく

--- GithubModUpdateCheckerConsole/Settings.cs
+++ GithubModUpdateCheckerConsole/Settings.cs
@@ -7,6 +7,6 @@
         public static Settings Instance { get; set; } = new Settings();
         public string BeatSaberExeFolderPath { get; set; } = @"C:\Program Files (x86)\Steam\steamapps\common\Beat Saber";
 
-        public string OAuthToken { get; set; } = "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";
+        public string OAuthToken { get; set; } = "";
     }
 }
